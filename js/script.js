cfaebd5c5d3fd40c87b870c9367b06b0cfaeef5a
username <anmolkumar3462@gmail.com>
2023-06-15 15:32:02 +0530

    javascript day 1

---
+++ js/script.js
@@ -0,0 +1,9 @@
+//Statement no 1.
+console.log("Hello from external file");
+var x = 10; //variable declaration and Initialization
+var y = 20; //variable declaration and Initialization
+console.log(x+y);
+console.log(x-y);
+console.log(x*y); //Astrick symbol means multiplication
+console.log(y-x);
+window.alert(x*y);
